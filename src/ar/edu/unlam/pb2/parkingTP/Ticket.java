4658b3a9689e3693c98afe1ccec60317e01cd809
ValenMelito <143010111+ValenMelito@users.noreply.github.com>
2024-05-28 01:46:34 +0000

    agrego cambios en la hoja tickets

--- src/ar/edu/unlam/pb2/parkingTP/Ticket.java
+++ src/ar/edu/unlam/pb2/parkingTP/Ticket.java
@@ -1,5 +1,93 @@
 package ar.edu.unlam.pb2.parkingTP;
 
-public class Ticket {
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Tickets {
+	
+	private Integer id;
+	private LocalDateTime fechaHoraEntrada;
+	private LocalDateTime fechaHoraSalida;
+	private Integer nroPlaza;
+	private Double costo;
+	private static final double HORA_COCHE = 2000;
+    private static final double HORA_MOTO = 1000;
+	
+	// en realidad se hace un ticket a la entrada ya a la salida se cierra el fechaHoraSalida pero esto es modelo de practica, lo mismo con el costo.
+	public Tickets(Integer id, LocalDateTime fechaHoraEntrada,LocalDateTime fechaHoraSalida, Integer nroPlaza, Double costo) {
+		this.id = id;
+		this.fechaHoraEntrada = fechaHoraEntrada;
+		this.fechaHoraSalida= fechaHoraSalida;
+		this.nroPlaza = nroPlaza;
+		this.costo = costo;
+	}
+	
+	
+	public double calcularCosto(Vehiculo vehiculo){
+		Double valorVehiculo=getValorVehiculo(vehiculo);
+		Duration tiempoEstadia=calculoTiempoEstadia();
+		
+		return 0.0;
+	}
+	
+	public Double getValorVehiculo(Vehiculo vehiculo){
+		String tipoVehiculo =vehiculo.getClass().getName();
+		Double valorDependiendoDeVehiculo=0;
+		switch(tipoVehiculo) {
+			case Coche:
+				valorDependiendoDeVehiculo=HORA_COCHE;
+				break;
+				
+			case Moto:
+				valorDependiendoDeVehiculo=HORA_MOTO;
+				break;
+		}
+		return valorDependiendoDeVehiculo;
+	}
+	
+	public Duration calculoTiempoEstadia(){
+		Duration tiempoEstadia = Duration.between(this.fechaHoraEntrada, this.fechaHoraSalida);
+		return tiempoEstadia;
+		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public Integer getNroPlaza() {
+		return nroPlaza;
+	}
+
+
+
+	public void setNroPlaza(Integer nroPlaza) {
+		this.nroPlaza = nroPlaza;
+	}
+
+
+
+	public LocalDateTime getFechaHoraEntrada() {
+		return fechaHoraEntrada;
+	}
+
+	public void setFechaHoraEntrada(LocalDateTime fechaHoraEntrada) {
+		this.fechaHoraEntrada = fechaHoraEntrada;
+	}
+
+	public LocalDateTime getFechaHoraSalida() {
+		return fechaHoraSalida;
+	}
+
+	public void setFechaHoraSalida(LocalDateTime fechaHoraSalida) {
+		this.fechaHoraSalida = fechaHoraSalida;
+	}
 
 }
+	
--- test/ar/edu/unlam/pb2/parkingTP/TicketTest.java
+++ test/ar/edu/unlam/pb2/parkingTP/TicketTest.java
@@ -6,9 +6,35 @@ import org.junit.Test;
 
 public class TicketTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+@Test
+	public void crearCorrectamenteUnTicket() {
+		 Integer id=1;
+		 LocalDateTime fechaHoraEntrada=LocalDateTime.of(2023, 5, 24, 22, 50, 0);
+		 LocalDateTime fechaHoraSalida=LocalDateTime.of(2023, 5, 24, 23, 50, 0);
+		 Integer nroPlaza=30;
+		 Double costo=10.5;
+		 Tickets ticketClient = new Tickets(id, fechaHoraEntrada, fechaHoraSalida, nroPlaza, costo);
+		 assertTrue(ticketClient.getId()==1);
 	}
+	
+	@Test
+	public void calcularCorrectamenteElTiempoDeEstadia() {
+		 Integer id=1;
+		 LocalDateTime fechaHoraEntrada=LocalDateTime.of(2023, 5, 24, 22, 50, 0);
+		 LocalDateTime fechaHoraSalida=LocalDateTime.of(2023, 5, 24, 23, 50, 0);
+		 Integer nroPlaza=30;
+		 Double costo=10.5;
+		 Tickets ticketClient = new Tickets(id, fechaHoraEntrada, fechaHoraSalida, nroPlaza, costo);
+		 Duration a=ticketClient.calculoTiempoEstadia();
+	     long minutos =(int) a.toMinutes();
+	     long segundos = a.getSeconds();
+	     assertTrue(minutos==60);
+	}
+	
+	/*
+	@Test
+	public void calcularCorrectamenteElTiempoDeEstadia() {
+		
+	}*/
 
 }
