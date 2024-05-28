001fa96f3d8b483d4e73611df7437652d4592cf5
ValenMelito <143010111+ValenMelito@users.noreply.github.com>
2024-05-28 02:14:35 +0000

    agrego la clase ticket y sus test y pruebas avanzadas

--- src/ar/edu/unlam/pb2/parkingTP/Ticket.java
+++ src/ar/edu/unlam/pb2/parkingTP/Ticket.java
@@ -3,18 +3,18 @@ package ar.edu.unlam.pb2.parkingTP;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class Tickets {
+public class Ticket {
 	
 	private Integer id;
 	private LocalDateTime fechaHoraEntrada;
 	private LocalDateTime fechaHoraSalida;
 	private Integer nroPlaza;
 	private Double costo;
-	private static final double HORA_COCHE = 2000;
+	private static final double HORA_COCHE = 2000.0;
     private static final double HORA_MOTO = 1000;
 	
 	// en realidad se hace un ticket a la entrada ya a la salida se cierra el fechaHoraSalida pero esto es modelo de practica, lo mismo con el costo.
-	public Tickets(Integer id, LocalDateTime fechaHoraEntrada,LocalDateTime fechaHoraSalida, Integer nroPlaza, Double costo) {
+	public Ticket(Integer id, LocalDateTime fechaHoraEntrada,LocalDateTime fechaHoraSalida, Integer nroPlaza, Double costo) {
 		this.id = id;
 		this.fechaHoraEntrada = fechaHoraEntrada;
 		this.fechaHoraSalida= fechaHoraSalida;
@@ -31,14 +31,15 @@ public class Tickets {
 	}
 	
 	public Double getValorVehiculo(Vehiculo vehiculo){
-		String tipoVehiculo =vehiculo.getClass().getName();
-		Double valorDependiendoDeVehiculo=0;
+		String tipoVehiculo =vehiculo.getClass().getSimpleName();
+		Double valorDependiendoDeVehiculo=0.0;
+		System.out.println(tipoVehiculo);
 		switch(tipoVehiculo) {
-			case Coche:
+			case "Auto":
 				valorDependiendoDeVehiculo=HORA_COCHE;
 				break;
 				
-			case Moto:
+			case "Bici":
 				valorDependiendoDeVehiculo=HORA_MOTO;
 				break;
 		}
--- test/ar/edu/unlam/pb2/parkingTP/TicketTest.java
+++ test/ar/edu/unlam/pb2/parkingTP/TicketTest.java
@@ -1,6 +1,9 @@
 package ar.edu.unlam.pb2.parkingTP;
 
 import static org.junit.Assert.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import org.junit.Test;
 
@@ -13,7 +16,7 @@ public class TicketTest {
 		 LocalDateTime fechaHoraSalida=LocalDateTime.of(2023, 5, 24, 23, 50, 0);
 		 Integer nroPlaza=30;
 		 Double costo=10.5;
-		 Tickets ticketClient = new Tickets(id, fechaHoraEntrada, fechaHoraSalida, nroPlaza, costo);
+		 Ticket ticketClient = new Ticket(id, fechaHoraEntrada, fechaHoraSalida, nroPlaza, costo);
 		 assertTrue(ticketClient.getId()==1);
 	}
 	
@@ -24,17 +27,26 @@ public class TicketTest {
 		 LocalDateTime fechaHoraSalida=LocalDateTime.of(2023, 5, 24, 23, 50, 0);
 		 Integer nroPlaza=30;
 		 Double costo=10.5;
-		 Tickets ticketClient = new Tickets(id, fechaHoraEntrada, fechaHoraSalida, nroPlaza, costo);
+		 Ticket ticketClient = new Ticket(id, fechaHoraEntrada, fechaHoraSalida, nroPlaza, costo);
 		 Duration a=ticketClient.calculoTiempoEstadia();
 	     long minutos =(int) a.toMinutes();
 	     long segundos = a.getSeconds();
 	     assertTrue(minutos==60);
 	}
 	
-	/*
+	
 	@Test
-	public void calcularCorrectamenteElTiempoDeEstadia() {
-		
-	}*/
+	public void calcularElCostoDeLaEstadia() {
+		Integer id=1;
+		LocalDateTime fechaHoraEntrada=LocalDateTime.of(2023, 5, 24, 22, 50, 0);
+		LocalDateTime fechaHoraSalida=LocalDateTime.of(2023, 5, 24, 23, 50, 0);
+		Integer nroPlaza=30;
+		Double costo=10.5;
+		Ticket ticketClient = new Ticket(id, fechaHoraEntrada, fechaHoraSalida, nroPlaza, costo);
+		Auto auto= new Auto(id);
+		double valor=ticketClient.getValorVehiculo(auto);
+		assertTrue(valor==2000.0);
+
+	}
 
 }
