2e4f988bcdb2bbcb715f511ed20d0f3631f8bef6
Nazaazul <nazaleonel246@gmail.com>
2024-05-29 07:27:21 -0300

    Update HistorialDelEstacionamiento.java

--- src/ar/edu/unlam/pb2/parkingTP/HistorialDelEstacionamiento.java
+++ src/ar/edu/unlam/pb2/parkingTP/HistorialDelEstacionamiento.java
@@ -1,5 +1,24 @@
 package ar.edu.unlam.pb2.parkingTP;
 
-public class HistorialDelEstacionamiento {
+import java.util.HashSet;
+import java.util.Set;
 
+public class HistorialDelEstacionamiento {
+    private Set<Ticket> historialTickets;
+
+    public HistorialDelEstacionamiento() {
+        this.historialTickets = new HashSet<>();
+    }
+
+    public void agregarTicket(Ticket ticket) {
+        this.historialTickets.add(ticket);
+    }
+
+    public Set<Ticket> getHistorialTickets() {
+        return historialTickets;
+    }
+
+    public void setHistorialTickets(Set<Ticket> historialTickets) {
+        this.historialTickets = historialTickets;
+    }
 }
